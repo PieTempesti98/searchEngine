30fc6ed1080db75700a81638d195e8c777f524a4
btessa99 <84031182+btessa99@users.noreply.github.com>
2022-12-05 10:11:02 +0100

    Updated CLI

--- cli/src/main/java/it/unipi/aide/mircv/start/Main.java
+++ cli/src/main/java/it/unipi/aide/mircv/start/Main.java
@@ -31,11 +31,14 @@ public class Main {
 
         String query;
 
-        for(;;) { // forever
-            System.out.println("What are you looking for? " + """
+        System.out.println("What are you looking for? " + """
             Please insert a query specifying your preferred mode:\s
             -c for conjunctive mode or -d for disjunctive mode. Here's an example:\s
-            This is a query example -c""");
+            This is a query example -c \s
+            Type "help" to get help or "break" to terminate the service""");
+
+        for(;;) {
+            System.out.println("What are you looking for? Type \"help\" to get help or \"break\" to terminate the service to terminate the service");
             query = sc.nextLine();
 
             //check if user didn't input an empty query
@@ -46,9 +49,34 @@ public class Main {
             }
 
             //get query text and mode.
-            // queryInfo[0] -> query text queryInfo[1] -> query mode
+            // queryInfo[0] -> query text or break or help , queryInfo[1] -> query mode
             String[] queryInfo = query.split("-");
-            if(queryInfo.length < 2){ //check if both text and mode were inputted
+
+            if(queryInfo.length == 1){
+
+                //check if break input is inputted
+                if(queryInfo[0].equals("break")){
+                    System.out.println("Bye.. Hope you have found everything you were looking for :)");
+                    break;
+                }
+
+                //check if help input is inputted
+                if(queryInfo[0].equals("help")){
+                    System.out.println("""
+                            Please insert a query specifying your preferred mode:
+                                        -c for conjunctive mode or -d for disjunctive mode. Here's an example:s
+                                        This is a query example -c s
+                                        Insert -h for help or -b to terminate the service""");
+                    continue;
+                }
+
+                //invalid input
+                System.out.println("The query you entered is in invalid format.");
+                continue;
+            }
+
+            //check if query mode is valid
+            if(!queryInfo[1].equals("c") && !queryInfo[1].equals("d")){
                 System.out.println("The query you entered is in invalid format.");
                 continue;
             }
