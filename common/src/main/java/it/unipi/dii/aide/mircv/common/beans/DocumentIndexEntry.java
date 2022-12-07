e7c4d08763506bd86499f666cdad1ce64980a818
Pietro Tempesti <58608244+PieTempesti98@users.noreply.github.com>
2022-12-07 16:33:48 +0100

    Created readFromDisk and WriteToDisk for the document index entry

--- common/src/main/java/it/unipi/dii/aide/mircv/common/beans/DocumentIndexEntry.java
+++ common/src/main/java/it/unipi/dii/aide/mircv/common/beans/DocumentIndexEntry.java
@@ -1,8 +1,17 @@
 package it.unipi.dii.aide.mircv.common.beans;
 
+import it.unipi.dii.aide.mircv.common.config.ConfigurationParameters;
+
 import java.io.IOException;
 import java.io.Serial;
 import java.io.Serializable;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 
 public class DocumentIndexEntry implements Serializable {
@@ -10,17 +19,32 @@ public class DocumentIndexEntry implements Serializable {
     /**
      * pid of a document
      * */
-    String pid;
+    private String pid;
 
     /**
     * docid of a document
     * */
-    int docid;
+    private int docid;
 
     /**
      * length of a documents in terms of number of terms
      * */
-    int docLen;
+    private int docLen;
+
+    /**
+     * Memory offset of the documentIndex file
+     */
+    private static long memOffset = 0;
+
+    /**
+     * Size of the pid on disk
+     */
+    private static final int PID_SIZE = 64;
+
+    /**
+     * Size of the document index entry on disk
+     */
+    private static final int ENTRY_SIZE = PID_SIZE + 4 + 4;
 
     /**
      * Constructor for the document index entry of a specific document
@@ -81,5 +105,89 @@ public class DocumentIndexEntry implements Serializable {
                 ", docid=" + docid +
                 ", docLen=" + docLen +
                 '}';
+    }
+
+    /**
+     * Write the document index entry on disk
+     * @return true if the operation was successful
+     */
+    public boolean writeToDisk(){
+        // try to open a file channel to the file of the inverted index
+        try (FileChannel fChan = (FileChannel) Files.newByteChannel(
+                Paths.get(ConfigurationParameters.getDocumentIndexPath()),
+                StandardOpenOption.WRITE,
+                StandardOpenOption.READ,
+                StandardOpenOption.CREATE)) {
+            // instantiation of MappedByteBuffer for the entry
+            MappedByteBuffer buffer = fChan.map(FileChannel.MapMode.READ_WRITE, memOffset, ENTRY_SIZE);
+
+            // Buffer not created
+            if(buffer == null)
+                return false;
+
+            // Create the CharBuffer with size = PID_SIZE
+            CharBuffer charBuffer = CharBuffer.allocate(PID_SIZE);
+            charBuffer.put(this.pid);
+            // Write the PID into file
+            buffer.put(StandardCharsets.UTF_8.encode(charBuffer));
+
+            // Write the docid into file
+            buffer.putInt(this.docid);
+            // Write the doclen into file
+            buffer.putInt(this.docLen);
+            // update memory offset
+            memOffset = memOffset + ENTRY_SIZE;
+
+            return true;
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    /**
+     * Read the document index entry from disk
+     * @param memoryOffset the memory offset from which we start reading
+     * @return true if the read is successful
+     */
+    public boolean readFromDisk(long memoryOffset){
+        // try to open a file channel to the file of the inverted index
+        try (FileChannel fChan = (FileChannel) Files.newByteChannel(
+                Paths.get(ConfigurationParameters.getDocumentIndexPath()),
+                StandardOpenOption.WRITE,
+                StandardOpenOption.READ,
+                StandardOpenOption.CREATE)) {
+
+            // instantiation of MappedByteBuffer for the PID read
+            MappedByteBuffer buffer = fChan.map(FileChannel.MapMode.READ_WRITE, memoryOffset, PID_SIZE);
+
+            // Buffer not created
+            if(buffer == null)
+                return false;
+
+            // Read from file into the charBuffer, then pass to the string
+            CharBuffer charBuffer = StandardCharsets.UTF_8.decode(buffer);
+            this.pid = charBuffer.toString();
+
+            // Instantiate the buffer for reading other information
+            buffer = fChan.map(FileChannel.MapMode.READ_WRITE, memoryOffset + PID_SIZE, ENTRY_SIZE - PID_SIZE);
+
+            // Buffer not created
+            if(buffer == null)
+                return false;
+
+            // read the docid
+            this.docid = buffer.getInt();
+            // read the doclen
+            this.docLen = buffer.getInt();
+
+            return true;
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 }
