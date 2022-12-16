a17bc121db622b775fcbdc1e445df1c0d3a426c1
Pietro Tempesti <58608244+PieTempesti98@users.noreply.github.com>
2022-12-16 19:57:31 +0100

    implemented variableByteCompressor and its tests

--- common/src/main/java/it/unipi/dii/aide/mircv/common/compression/VariableByteCompressor.java
+++ common/src/main/java/it/unipi/dii/aide/mircv/common/compression/VariableByteCompressor.java
@@ -1,6 +1,40 @@
 package it.unipi.dii.aide.mircv.common.compression;
 
+import java.util.ArrayList;
+
+import static java.lang.Math.log;
+
 public class VariableByteCompressor {
+
+    /**
+     * Method for compressing a single integer
+     * @param toBeCompressed the integer to be compressed
+     * @return the compressed representation of the input number
+     */
+    private static byte[] integerCompression(int toBeCompressed){
+
+        // case of the number 0
+        if(toBeCompressed == 0){
+            return new byte[]{0};
+        }
+
+        // compute the number of bytes needed
+        int numBytes = (int) (log(toBeCompressed) / log(128)) + 1;
+
+        // allocate the output byte array
+        byte[] output = new byte[numBytes];
+
+        // for each position (starting from the least significant) set the correct bytes
+        // then divide the number by 128 to prepare the next setting
+        for(int position = numBytes - 1; position >= 0; position--){
+            output[position] = (byte) (toBeCompressed % 128);
+            toBeCompressed /= 128;
+        }
+
+        // set the most significant bit of the least significant byte to 1
+        output[numBytes - 1] += 128;
+        return output;
+    }
 
     /**
      * Method to compress an array of integers into an array of bytes using Unary compression algorithm
@@ -8,12 +42,18 @@ public class VariableByteCompressor {
      * @return an array containing the compressed bytes
      */
     public static byte[] integerArrayCompression(int[] toBeCompressed){
+        ArrayList<Byte> compressedArray = new ArrayList<>();
 
-        byte[] compressedArray = new byte[nBytes];
+        for(int number: toBeCompressed){
+            for(byte elem: integerCompression(number))
+                compressedArray.add(elem);
+        }
 
-        // TODO: IMPLEMENTATION
+        byte[] output = new byte[compressedArray.size()];
+        for(int i = 0; i < compressedArray.size(); i++)
+            output[i] = compressedArray.get(i);
 
-        return compressedArray;
+        return output;
     }
 
     /**
@@ -25,7 +65,31 @@ public class VariableByteCompressor {
     public static int[] integerArrayDecompression(byte[] toBeDecompressed, int totNums){
         int[] decompressedArray = new int[totNums];
 
-        // TODO: IMPLEMENTATION
+        // integer that I'm processing
+        int decompressedNumber = 0;
+
+        // count of the processed numbers (used also as a pointer in the output array)
+        int alreadyDecompressed = 0;
+
+        for(byte elem: toBeDecompressed){
+            if((elem & 0xff) < 128)
+                // not the termination byte, shift the actual number and insert the new byte
+                decompressedNumber = 128 * decompressedNumber + elem;
+            else{
+                // termination byte, remove the 1 at the MSB and then append the byte to the number
+                decompressedNumber = 128 * decompressedNumber + ((elem - 128) & 0xff);
+
+                // save the number in the output array
+                decompressedArray[alreadyDecompressed] = decompressedNumber;
+
+                // increase the number of processed numbers
+                alreadyDecompressed ++;
+
+                //reset the variable for the next number to decompress
+                decompressedNumber = 0;
+            }
+        }
+
         return decompressedArray;
     }
 }
---
+++ common/src/test/java/it/unipi/dii/aide/mircv/common/compression/VariableByteCompressorTest.java
@@ -0,0 +1,22 @@
+package it.unipi.dii.aide.mircv.common.compression;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VariableByteCompressorTest {
+
+    @Test
+    void integerArrayCompression() {
+        assertArrayEquals(new byte[]{(byte) 133}, VariableByteCompressor.integerArrayCompression(new int[]{5}));
+        assertArrayEquals(new byte[]{(byte) 6, (byte) 184}, VariableByteCompressor.integerArrayCompression(new int[]{824}));
+        assertArrayEquals(new byte[]{(byte) 6, (byte) 184,(byte) 133}, VariableByteCompressor.integerArrayCompression(new int[]{824, 5}));
+    }
+
+    @Test
+    void integerArrayDecompression() {
+        assertArrayEquals(new int[]{5}, VariableByteCompressor.integerArrayDecompression(new byte[]{(byte) 133}, 1));
+        assertArrayEquals(new int[]{824}, VariableByteCompressor.integerArrayDecompression(new byte[]{(byte) 6, (byte) 184}, 1));
+        assertArrayEquals(new int[]{824, 5}, VariableByteCompressor.integerArrayDecompression(new byte[]{(byte) 6, (byte) 184,(byte) 133}, 2));
+    }
+}
